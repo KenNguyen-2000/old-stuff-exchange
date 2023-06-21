808d1993affd37c37c665af6f18c49de691b4376
Nguyen Kien <ngkien299@gmail.com>
2023-06-22 00:19:53 +0700

    Export folder screens

--- client/src/screens/index.ts
+++ client/src/screens/index.ts
@@ -1,5 +1,17 @@
 import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
 import RegisterScreen from './RegisterScreen';
+import ProfileScreen from './ProfileScreen';
+import OrderScreen from './OrderScreen';
+import ChatScreen from './ChatScreen';
+import ItemDetailScreen from './ItemDetailScreen';
 
-export {LoginScreen, HomeScreen, RegisterScreen};
+export {
+  LoginScreen,
+  HomeScreen,
+  RegisterScreen,
+  ProfileScreen,
+  OrderScreen,
+  ChatScreen,
+  ItemDetailScreen,
+};
