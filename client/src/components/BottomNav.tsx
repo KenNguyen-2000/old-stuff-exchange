32ba829aae22a2b3669fc4ebe70e470468a3ac8d
Nguyen Kien <ngkien299@gmail.com>
2023-06-22 00:20:37 +0700

    Create Material Bottom Tab bar for HomeScreen, ChatScreen, OrderScreen, ProfileScreen

---
+++ client/src/components/BottomNav.tsx
@@ -0,0 +1,92 @@
+import { StyleSheet } from 'react-native';
+import React from 'react';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { ChatScreen, HomeScreen, OrderScreen, ProfileScreen } from '../screens';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useTheme } from 'react-native-paper';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const BottomNav = () => {
+  const theme = useTheme();
+
+  return (
+    <Tab.Navigator
+      initialRouteName='login'
+      activeColor={theme.colors.primary}
+      barStyle={styles.bar__wrapper}
+    >
+      <Tab.Screen
+        name='home'
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <MaterialCommunityIcons
+              name={focused ? 'home' : 'home-outline'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Order'
+        component={OrderScreen}
+        options={{
+          tabBarLabel: 'Order',
+          tabBarIcon: ({ color, focused }) => (
+            <MaterialCommunityIcons
+              name={
+                focused
+                  ? 'clipboard-multiple'
+                  : 'clipboard-text-multiple-outline'
+              }
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Chat'
+        component={ChatScreen}
+        options={{
+          tabBarLabel: 'Messages',
+          tabBarIcon: ({ color, focused }) => (
+            <MaterialCommunityIcons
+              name={focused ? 'chat' : 'chat-outline'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, focused }) => (
+            <MaterialCommunityIcons
+              name={focused ? 'account' : 'account-outline'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomNav;
+
+const styles = StyleSheet.create({
+  bar__wrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    maxHeight: 60,
+    backgroundColor: '#fff',
+  },
+});
