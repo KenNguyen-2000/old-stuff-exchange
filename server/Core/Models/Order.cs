2152b9c124a8f487d946821f180db51b25fd4532
Nguyen Kien <ngkien299@gmail.com>
2023-06-22 17:12:26 +0700

    Fix attribute

--- server/Core/Models/Order.cs
+++ server/Core/Models/Order.cs
@@ -20,11 +20,8 @@ namespace Core.Models
         public Guid ItemId { get; set; }
         public Item Item { get; set; }
         [Required]
-        public Guid SellerId { get; set; }
-        public User Seller { get; set; }
-        [Required]
-        public Guid BuyerId { get; set; }
-        public User Buyer { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
         public OrderStatus Status { get; set; } = OrderStatus.In_Progress;
     }
 }
