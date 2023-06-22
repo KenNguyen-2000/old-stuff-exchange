1277d266eaa5091802f901b4cad368ad32db5a77
Nguyen Kien <ngkien299@gmail.com>
2023-06-22 17:13:19 +0700

    Fix context

--- server/Infrastructure/Context/OldStuffExchangeContext.cs
+++ server/Infrastructure/Context/OldStuffExchangeContext.cs
@@ -12,7 +12,10 @@ namespace Infrastructure.Context
     public class OldStuffExchangeContext : DbContext
     {
         public OldStuffExchangeContext(DbContextOptions<OldStuffExchangeContext> options)
-        : base(options) { }
+        : base(options)
+        {
+
+        }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Item> Items { get; set; }
@@ -39,12 +42,8 @@ namespace Infrastructure.Context
                 .HasForeignKey(b => b.UserId);
             modelBuilder.Entity<User>()
                 .HasMany(u => u.Orders)
-                .WithOne(o => o.Buyer)
-                .HasForeignKey(u => u.BuyerId);
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.Orders)
-                .WithOne(o => o.Seller)
-                .HasForeignKey(u => u.SellerId);
+                .WithOne(o => o.User)
+                .HasForeignKey(o => o.UserId);
 
             modelBuilder.Entity<Item>()
                 .HasMany(i => i.Reviews)
@@ -55,6 +54,10 @@ namespace Infrastructure.Context
                 .WithOne(b => b.Item)
                 .HasForeignKey<Bill>(b => b.ItemId)
                 .IsRequired();
+            modelBuilder.Entity<Item>()
+                .HasOne(i => i.User)
+                .WithMany(u => u.Items)
+                .HasForeignKey(i => i.UserId);
 
 
             modelBuilder.Entity<Bill>()
@@ -63,17 +66,9 @@ namespace Infrastructure.Context
                 .HasForeignKey<Bill>(b => b.ItemId);
 
             modelBuilder.Entity<Order>()
-                .HasOne(e => e.Item)
+                .HasOne(o => o.Item)
                 .WithOne(i => i.Order)
-                .HasForeignKey<Order>(e => e.ItemId);
-            modelBuilder.Entity<Order>()
-                .HasOne(e => e.Seller)
-                .WithOne(u => u.Order)
-                .HasForeignKey<Order>(e => e.SellerId);
-            modelBuilder.Entity<Order>()
-                .HasOne(e => e.Buyer)
-                .WithOne(u => u.Order)
-                .HasForeignKey<Order>(e => e.BuyerId);
+                .HasForeignKey<Order>(o => o.ItemId);
         }
     }
 }
