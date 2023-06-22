3cdd10443782f4f0309b5295aedf7d49b14e9d5b
Nguyen Kien <ngkien299@gmail.com>
2023-06-22 17:11:16 +0700

    Add JsonOptionSeriabl ignore cycle reference

--- server/WebAPI/Program.cs
+++ server/WebAPI/Program.cs
@@ -6,13 +6,17 @@ using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using System.Configuration;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddAplication(builder.Configuration);
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
