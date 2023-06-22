fc99874818e4eaa38649563fe68aaf9413a0922c
Nguyen Kien <ngkien299@gmail.com>
2023-06-22 11:49:38 +0700

    Add useUserInfo hooks to get user information

--- client/src/screens/ProfileScreen.tsx
+++ client/src/screens/ProfileScreen.tsx
@@ -1,44 +1,76 @@
 import { StyleSheet, View, ScrollView } from 'react-native';
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Text, List, MD3Colors } from 'react-native-paper';
+import { Text, List, MD3Colors, Button } from 'react-native-paper';
 import {
   ProfileHeader,
   ProfileSection,
 } from '../components/screens/ProfileScreen';
+import useUserInfo from '../hooks/useUserInfo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useAppDispatch, useAppSelector } from '../redux/reduxHook';
+import { setUser } from '../redux/slices/userSlice';
 
 interface IProfileScreen
   extends NativeStackScreenProps<any, 'Profile', 'mystack'> {}
 
-const ProfileScreen = ({ navigation }: IProfileScreen) => {
+const ProfileScreen = ({ navigation, route }: IProfileScreen) => {
+  const userInfo = useAppSelector((state) => state.user.user);
+
+  const dispatch = useAppDispatch();
+
+  const handleLogout = async () => {
+    await AsyncStorage.removeItem('token');
+    dispatch(setUser(null));
+    navigation.navigate('Login');
+  };
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, [navigation]);
+  console.log('rerender');
 
   return (
-    <ScrollView
-      style={styles.scroll__wrapper}
-      showsVerticalScrollIndicator={false}
-    >
-      <View style={styles.wrapper}>
-        <ProfileHeader navigation={navigation} />
-        <ProfileSection />
-      </View>
-    </ScrollView>
+    <View style={styles.wrapper}>
+      <ScrollView
+        style={styles.scroll__wrapper}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.container}>
+          <ProfileHeader
+            navigation={navigation}
+            route={route}
+            userInfo={userInfo}
+          />
+          <ProfileSection />
+          <Button
+            mode='contained'
+            style={{ marginHorizontal: 8, borderRadius: 4 }}
+            onPress={handleLogout.bind(null)}
+          >
+            Log out
+          </Button>
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
 export default ProfileScreen;
 
 const styles = StyleSheet.create({
-  scroll__wrapper: {
+  wrapper: {
     flex: 1,
     backgroundColor: '#ebebeb',
   },
-  wrapper: {
+  scroll__wrapper: {
+    flex: 1,
+  },
+  container: {
     display: 'flex',
     gap: 10,
+    paddingBottom: 40,
   },
 });
