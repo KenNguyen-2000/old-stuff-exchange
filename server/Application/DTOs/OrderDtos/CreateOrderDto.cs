4571c58dda4ffee48354f9327b9f66c140b369b4
Nguyen Kien <ngkien299@gmail.com>
2023-06-22 17:12:54 +0700

    Fix attribute

--- server/Application/DTOs/OrderDtos/CreateOrderDto.cs
+++ server/Application/DTOs/OrderDtos/CreateOrderDto.cs
@@ -5,8 +5,7 @@ namespace Application.DTOs.OrderDtos
     public class CreateOrderDtos
     {
         public Guid ItemId { get; set; }
-        public Guid SellerId { get; set; }
-        public Guid BuyerId { get; set; }
-        public OrderStatus Status { get; set; }
+        public Guid UserId { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.In_Progress;
     }
 }
