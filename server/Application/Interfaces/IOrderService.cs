f99bf684b06f0553ea3615051002c5a08efa0ff8
Nguyen Kien <ngkien299@gmail.com>
2023-06-22 14:19:33 +0700

    Init OrderService interface

---
+++ server/Application/Interfaces/IOrderService.cs
@@ -0,0 +1,19 @@
+using System.Linq.Expressions;
+using Application.DTOs;
+using Application.DTOs.OrderDtos;
+using Core.Models;
+
+namespace Application.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Response<Order>> AddAsync(CreateOrderDtos createOrderDtos);
+        Task<Response<OrderDtos>> GetListAsync();
+        Task<Response<OrderDtos>> GetByIdAsync(Guid id);
+        Task<Response<Item>> UpdateAsync(OrderDtos orderDtos);
+        Task<Response<string>> DeleteAsync(Guid id);
+        Task<Response<OrderDtos>> GetAsync(Expression<Func<Order, bool>> filter);
+
+        Task<Response<OrderDtos>> UpdateStatusAsync(ChangeOrderStatusDto changeOrderStatusDto);
+    }
+}
