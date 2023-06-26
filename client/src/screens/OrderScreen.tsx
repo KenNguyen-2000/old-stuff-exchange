b2266db63d7f758c97aa1399ba53ce18f75b39e0
Nguyen Kien <ngkien299@gmail.com>
2023-06-27 00:15:11 +0700

    Add screens

--- client/src/screens/OrderScreen.tsx
+++ client/src/screens/OrderScreen.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import MySafeArea from '../components/MySafeArea';
 
 const OrderScreen = () => {
   return (
-    <View>
+    <MySafeArea>
       <Text>OrderScreen</Text>
-    </View>
+    </MySafeArea>
   );
 };
 
