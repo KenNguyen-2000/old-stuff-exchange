558f2699beb68815f2d8b01c3c8693125006ba3d
Nguyen Kien <ngkien299@gmail.com>
2023-07-01 10:31:15 +0700

    Fix the override style

--- client/src/components/MySafeArea.tsx
+++ client/src/components/MySafeArea.tsx
@@ -5,29 +5,32 @@ import {
   Text,
   View,
   ViewProps,
+  ViewStyle,
 } from 'react-native';
 import React from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface IMySafeArea extends ViewProps {}
 
-const MySafeArea = (props: IMySafeArea) => {
+const MySafeArea = ({ children, style }: IMySafeArea) => {
   const insets = useSafeAreaInsets();
-  return (
-    <View
-      style={[
-        {
-          flex: 1,
-          paddingTop:
-            Platform.OS === 'ios' ? insets.top : StatusBar.currentHeight,
-        },
-        props.style,
-      ]}
-      {...props}
-    >
-      {props.children}
-    </View>
-  );
+  const currentPaddingTop =
+    Platform.OS === 'ios' ? insets.top : StatusBar.currentHeight;
+  const { paddingTop, paddingVertical, ...otherStyles } = (style as any) || {};
+
+  const updatedPaddingTop =
+    paddingTop !== undefined
+      ? currentPaddingTop + paddingTop
+      : currentPaddingTop;
+  const updatedStyle = {
+    ...otherStyles,
+    paddingTop: updatedPaddingTop,
+    paddingVertical:
+      paddingVertical !== undefined
+        ? updatedPaddingTop + paddingVertical
+        : undefined,
+  };
+  return <View style={[{ flex: 1 }, updatedStyle]}>{children}</View>;
 };
 
 export default MySafeArea;
