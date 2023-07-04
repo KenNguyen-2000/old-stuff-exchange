7347fa9f191fc610bcdb999be0e3b86f0d2b4c33
Nguyen Kien <ngkien299@gmail.com>
2023-07-05 00:42:18 +0700

    Fix circular dependencies injections error

--- server/Infrastructure/ServicesRegistration.cs
+++ server/Infrastructure/ServicesRegistration.cs
@@ -17,6 +17,7 @@ namespace Infrastructure
                 opt.EnableSensitiveDataLogging();
             });
 
+
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IItemRepository, ItemRepository>();
             services.AddScoped<IReviewRepository, ReviewRepository>();
