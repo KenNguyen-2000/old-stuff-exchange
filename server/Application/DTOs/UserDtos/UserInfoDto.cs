44c50fb2d402be9f89b06ca60930378d156476f1
Nguyen Kien <ngkien299@gmail.com>
2023-07-07 18:51:13 +0700

    Remove redundunt fields

--- server/Application/DTOs/UserDtos/UserInfoDto.cs
+++ server/Application/DTOs/UserDtos/UserInfoDto.cs
@@ -19,7 +19,7 @@ namespace Application.DTOs.UserDtos
         public string PhoneNumber { get; set; }
         public bool Gender { get; set; }
 
-        public string Dob { get; set; }
+        public DateTime Dob { get; set; }
         public string Address { get; set; }
         public double Points { get; set; }
         public string Role { get; set; }
--- server/Application/DTOs/UserDtos/UserUpdateDto.cs
+++ server/Application/DTOs/UserDtos/UserUpdateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,14 @@ namespace Application.DTOs.UserDtos
 {
     public class UserUpdateDto
     {
+        [Required]
         public int Id { get; set; }
         public string FullName { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        public bool IsEmailConfirmed { get; set; }
         public string PhoneNumber { get; set; }
         public bool Gender { get; set; }
-        public string Dob { get; set; }
+        public DateTime Dob { get; set; }
         public string Address { get; set; }
-        public int Points { get; set; }
-        public string Role { get; set; }
     }
 }
