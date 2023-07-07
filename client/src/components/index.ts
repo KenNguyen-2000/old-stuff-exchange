5c78b192c0e645e29c20ce10603237594bc30ecd
Nguyen Kien <ngkien299@gmail.com>
2023-07-08 03:14:56 +0700

    export AlertDialog

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,6 @@
 import BottomNav from './BottomNav';
 import ItemCard from './ItemCard';
 import SearchHeader from './SearchHeader';
+import AlertDialog from './AlertDialog';
 
-export { BottomNav, ItemCard, SearchHeader };
+export { BottomNav, ItemCard, SearchHeader, AlertDialog };
