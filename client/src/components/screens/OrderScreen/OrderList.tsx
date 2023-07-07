4ebcee8c1337086312f87f8b19f83292faf4fb2a
Nguyen Kien <ngkien299@gmail.com>
2023-07-08 03:13:58 +0700

    Fetch order api and set global state in redux

--- client/src/components/screens/OrderScreen/OrderList.tsx
+++ client/src/components/screens/OrderScreen/OrderList.tsx
@@ -1,7 +1,12 @@
 import { StyleSheet, ScrollView, View } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import OrderCard from './OrderCard';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useAppDispatch, useAppSelector } from '../../../redux/reduxHook';
+import { useIsFocused } from '@react-navigation/native';
+import { fetchOrderList } from '../../../redux/thunks/orders.thunk';
+import { IOrderDto } from '../../../interfaces/dtos/order.dto';
+import { ActivityIndicator } from 'react-native-paper';
 
 interface IOrderList extends NativeStackScreenProps<any, 'Order', 'my-stack'> {
   showBottomSheet: any;
@@ -12,15 +17,34 @@ const OrderList: React.FC<IOrderList> = ({
   navigation,
   route,
 }) => {
+  const dispatch = useAppDispatch();
+  const isFocused = useIsFocused();
+
+  const orders = useAppSelector((state) => state.orders.orders);
+  const ordersLoading = useAppSelector((state) => state.orders.isLoading);
+  useEffect(() => {
+    const fetchOrders = () => {
+      dispatch(fetchOrderList());
+    };
+    if (isFocused) fetchOrders();
+  }, [isFocused]);
   return (
     <View>
-      <ScrollView>
-        <OrderCard
-          navigation={navigation}
-          route={route}
-          showBottomSheet={showBottomSheet}
-        />
-      </ScrollView>
+      {ordersLoading ? (
+        <ActivityIndicator animating={true} />
+      ) : (
+        <ScrollView>
+          {orders?.map((order: IOrderDto) => (
+            <OrderCard
+              key={order.id}
+              navigation={navigation}
+              route={route}
+              showBottomSheet={showBottomSheet}
+              data={order}
+            />
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
 };
