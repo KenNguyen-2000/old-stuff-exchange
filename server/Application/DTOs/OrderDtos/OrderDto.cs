6d8a4b3f844906328dcd758617b4761092c89121
Nguyen Kien <ngkien299@gmail.com>
2023-07-07 18:51:30 +0700

    Extend AuditableEntity

--- server/Application/DTOs/OrderDtos/OrderDto.cs
+++ server/Application/DTOs/OrderDtos/OrderDto.cs
@@ -1,13 +1,16 @@
+using Application.DTOs.ItemDtos;
 using Core.Models;
+using System.Text.Json.Serialization;
 
 namespace Application.DTOs.OrderDtos
 {
     public class OrderDtos
     {
         public int Id { get; set; }
-        public int ItemId { get; set; }
+        public ItemDto Item { get; set; }
         public int SellerId { get; set; }
         public int BuyerId { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public OrderStatus Status { get; set; }
     }
 }
