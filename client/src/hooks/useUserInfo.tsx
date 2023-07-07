2d8acf69a546b15b29755269becd64087d718eca
Nguyen Kien <ngkien299@gmail.com>
2023-07-08 03:11:34 +0700

    Fix useUserInfo hook logic

--- client/src/hooks/useUserInfo.tsx
+++ client/src/hooks/useUserInfo.tsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import jwtDecode from 'jwt-decode';
 import { getUserInfo } from '../services/user.service';
 import { IUserInfo } from '../interfaces/dtos';
+import * as SecureStore from 'expo-secure-store';
 
 const useUserInfo = () => {
   const [userInfo, setUserInfo] = useState<IUserInfo | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const token = await AsyncStorage.getItem('token');
+      const token = await SecureStore.getItemAsync('token');
 
       if (!token) {
         setUserInfo(null);
