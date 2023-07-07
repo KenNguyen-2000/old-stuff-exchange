05b7bf261bd8d470f758b030383496a83b7e0a18
Nguyen Kien <ngkien299@gmail.com>
2023-07-08 03:10:29 +0700

    Extend auditable & add enum OrderStatus & add Order dtos

--- client/src/interfaces/dtos/order.dto.ts
+++ client/src/interfaces/dtos/order.dto.ts
@@ -1,3 +1,28 @@
+import { IAuditable } from './auditable.dto';
+import { OrderStatus } from '../enums/order.enum';
+import { IItemDto } from './item.dto';
+
+interface IUserOrderDto {
+  id: number;
+  fullName: string;
+  email?: string;
+  phoneNumber?: string;
+  gender: boolean;
+  dob: Date;
+  address: string;
+}
+
+export interface IOrderDto extends IAuditable {
+  item: IItemDto;
+  user: IUserOrderDto;
+  status: OrderStatus;
+}
+
 export interface ICreateOrder {
-  itemId: string;
+  itemId: number;
+}
+
+export interface IChangeOrderStatus {
+  id: number;
+  status: OrderStatus;
 }
