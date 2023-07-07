536cf73edae0ea3270a502fb5ad2be8fdc4ab1d4
Nguyen Kien <ngkien299@gmail.com>
2023-07-08 03:16:14 +0700

    Add change status & get list services

--- client/src/services/order.service.ts
+++ client/src/services/order.service.ts
@@ -1,9 +1,23 @@
+import { IChangeOrderStatus } from '../interfaces/dtos/order.dto';
 import interceptor from './interceptor';
 
-export const makeAnOrder = async (itemId: string) => {
+export const makeAnOrder = async (itemId: number) => {
   const res = await interceptor.post('/order', {
     itemId: itemId,
   });
 
+  return res.data;
+};
+
+export const changeOrderStatus = async ({ id, status }: IChangeOrderStatus) => {
+  const res = await interceptor.patch(`/order/${id}`, {
+    status: status,
+  });
+
+  return res.data;
+};
+
+export const getOrderList = async () => {
+  const res = await interceptor.get('/order');
   return res.data;
 };
