874303ddd58bf966528395696e17f582b399f99c
Nguyen Kien <ngkien299@gmail.com>
2023-07-09 15:55:16 +0700

    Fix circular import

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,6 +1,5 @@
 import BottomNav from './BottomNav';
 import ItemCard from './ItemCard';
 import SearchHeader from './SearchHeader';
-import AlertDialog from './AlertDialog';
 
-export { BottomNav, ItemCard, SearchHeader, AlertDialog };
+export { BottomNav, ItemCard, SearchHeader };
