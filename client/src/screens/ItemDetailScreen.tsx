12843d97ac0a32d61d16c6c88357df8e7d85b4b9
Nguyen Kien <ngkien299@gmail.com>
2023-07-09 16:01:22 +0700

    Add Carousel on Images

--- client/src/screens/ItemDetailScreen.tsx
+++ client/src/screens/ItemDetailScreen.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, View, ImageBackground, ScrollView } from 'react-native';
-import React, { useCallback, useEffect, useLayoutEffect, useMemo } from 'react';
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  ScrollView,
+  Dimensions,
+  Image,
+} from 'react-native';
+import React, { useState, useEffect, useLayoutEffect, useMemo } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import {
   Button,
+  Dialog,
   IconButton,
   MD3Colors,
+  Portal,
   Text,
   useTheme,
 } from 'react-native-paper';
@@ -16,12 +25,14 @@ import { deleteOldStuff } from '../redux/slices/itemListSlice';
 import { makeAnOrder } from '../services/order.service';
 import { IItemDto, IUserInfo } from '../interfaces/dtos';
 import { fetchUserInfo } from '../redux/thunks/user.thunk';
+import Carousel from 'react-native-reanimated-carousel';
 
 type RootStackParamList = {
   ItemDetail: {
     item: IItemDto;
   };
   Login: any;
+  EditItem: any;
 };
 
 interface IItemDetailScreen
@@ -34,6 +45,7 @@ const ItemDetailScreen = ({ navigation, route }: IItemDetailScreen) => {
   const dispatch = useAppDispatch();
   const oldStuffs = useAppSelector((state) => state.items.oldStuffs);
   const userInfo: IUserInfo | null = useAppSelector((state) => state.user.user);
+  const [visible, setVisible] = useState(false);
 
   const handleMakeExchange = async (itemId: number) => {
     try {
@@ -56,8 +68,32 @@ const ItemDetailScreen = ({ navigation, route }: IItemDetailScreen) => {
     }
   };
 
+  const AlertModal = () => {
+    return (
+      <Portal>
+        <Dialog visible={visible} onDismiss={() => setVisible(false)}>
+          <Dialog.Title>Cancel Order</Dialog.Title>
+          <Dialog.Content>
+            <Text variant='bodyMedium'>Are sure about delete this item?</Text>
+            <Text>This action cannot be reversed</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              textColor={MD3Colors.error40}
+              onPress={handleDeleteItem.bind(null, item.id)}
+            >
+              Confirm
+            </Button>
+            <Button onPress={() => setVisible(false)}>Dismiss</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    );
+  };
+
   useEffect(() => {
-    if (userInfo === null) dispatch(fetchUserInfo());
+    const fetchUser = async () => await dispatch(fetchUserInfo());
+    if (!userInfo) fetchUser();
   }, []);
 
   useLayoutEffect(() => {
@@ -69,25 +105,44 @@ const ItemDetailScreen = ({ navigation, route }: IItemDetailScreen) => {
   return (
     <ScrollView style={styles.wrapper}>
       <Pressable style={styles.header__wrapper}>
-        <ImageBackground
-          source={{
-            uri:
-              item.images.length > 0
-                ? item.images[0].imageUri
-                : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png',
-          }}
-          resizeMode='cover'
-          style={styles.header__wrapper}
-        >
-          <IconButton
-            icon={'keyboard-backspace'}
-            mode='outlined'
-            style={styles.back__icon}
-            containerColor={MD3Colors.error60}
-            iconColor={MD3Colors.error100}
-            size={24}
-            onPress={() => navigation.pop()}
-          />
+        <Carousel
+          loop
+          width={Dimensions.get('window').width}
+          height={(Dimensions.get('window').width * 4) / 3}
+          autoPlay={true}
+          autoPlayInterval={3000}
+          data={item.images}
+          scrollAnimationDuration={2000}
+          renderItem={({ item, index }) => (
+            <View
+              style={[
+                {
+                  flex: 1,
+                  justifyContent: 'center',
+                },
+              ]}
+              key={item.id}
+            >
+              <Image
+                source={{
+                  uri: item.imageUri,
+                }}
+                resizeMode='cover'
+                style={styles.header__image}
+              />
+            </View>
+          )}
+        />
+        <IconButton
+          icon={'keyboard-backspace'}
+          mode='outlined'
+          style={styles.back__icon}
+          containerColor={MD3Colors.error60}
+          iconColor={MD3Colors.error100}
+          size={24}
+          onPress={() => navigation.pop()}
+        />
+        {userInfo && userInfo.id !== item.user.id && (
           <IconButton
             icon={'shopping'}
             mode='contained'
@@ -97,8 +152,9 @@ const ItemDetailScreen = ({ navigation, route }: IItemDetailScreen) => {
             style={styles.shopping__icon}
             onPress={handleMakeExchange.bind(null, item.id)}
           />
-        </ImageBackground>
+        )}
       </Pressable>
+
       <View style={styles.body__wrapper}>
         <Text style={styles.item__title} variant='headlineMedium'>
           {item.name}
@@ -106,18 +162,15 @@ const ItemDetailScreen = ({ navigation, route }: IItemDetailScreen) => {
         <Text variant='titleMedium' style={{ marginBottom: 12 }}>
           {item.price} Points
         </Text>
-        <Text style={styles.item__description}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset{' '}
+        <Text style={styles.item__title} variant='headlineSmall'>
+          About Item
         </Text>
+        <Text style={styles.item__description}>{item.description}</Text>
 
         <View>
-          <Text variant='headlineSmall'>Others</Text>
+          <Text style={styles.item__title} variant='headlineSmall'>
+            Others
+          </Text>
           <FlatList
             data={oldStuffs.filter((stuff) => stuff.id !== item.id)}
             horizontal={true}
@@ -152,12 +205,12 @@ const ItemDetailScreen = ({ navigation, route }: IItemDetailScreen) => {
           />
         </View>
 
-        {(userInfo == null || userInfo.id !== item.user.id) && (
+        {(!userInfo || userInfo.id !== item.user.id) && (
           <Button
             style={{ marginTop: 16, borderRadius: 8 }}
             mode='contained'
             onPress={
-              userInfo === null
+              !userInfo
                 ? () => navigation.navigate('Login')
                 : handleMakeExchange.bind(null, item.id)
             }
@@ -171,7 +224,11 @@ const ItemDetailScreen = ({ navigation, route }: IItemDetailScreen) => {
             <Button
               style={{ marginTop: 16, borderRadius: 8 }}
               mode='contained'
-              onPress={handleMakeExchange.bind(null, item.id)}
+              onPress={() =>
+                navigation.navigate('EditItem', {
+                  item: item,
+                })
+              }
               buttonColor={theme.colors.tertiary}
             >
               Edit
@@ -179,7 +236,7 @@ const ItemDetailScreen = ({ navigation, route }: IItemDetailScreen) => {
             <Button
               style={{ marginTop: 8, borderRadius: 8 }}
               mode='contained'
-              onPress={handleDeleteItem.bind(null, item.id)}
+              onPress={() => setVisible(true)}
               buttonColor={theme.colors.error}
             >
               Delete
@@ -187,6 +244,7 @@ const ItemDetailScreen = ({ navigation, route }: IItemDetailScreen) => {
           </>
         )}
       </View>
+      {AlertModal()}
     </ScrollView>
   );
 };
@@ -205,6 +263,13 @@ const styles = StyleSheet.create({
     zIndex: 2,
     borderBottomWidth: 1.2,
     borderBottomColor: '#d0d0d0',
+  },
+  header__image: {
+    width: '100%',
+    aspectRatio: 3 / 4,
+    position: 'relative',
+    zIndex: 2,
+    borderBottomWidth: 1.2,
   },
   back__icon: {
     position: 'absolute',
