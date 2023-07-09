4cac5afc2fd473a41ca9b50d477eea1f435ed3ad
Nguyen Kien <ngkien299@gmail.com>
2023-07-09 23:09:52 +0700

    Define IChatService

---
+++ server/Application/Interfaces/IChatService.cs
@@ -0,0 +1,23 @@
+﻿using Application.DTOs.ItemDtos;
+using Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs.ChatDtos;
+using Core.Models;
+
+namespace Application.Interfaces
+{
+    public interface IChatService
+    {
+        public Task<MessageDto> SendMessage(SendMessageDto sendMessageDto, int recieverId);
+        public Task<IEnumerable<MessageDto>> GetListAsync(int roomId, int senderId);
+        public Task JoinRoom(int roomId);
+        public Task LeaveRoom(int roomId);
+        public Task<IEnumerable<RoomChatDto>> GetListRoomChatAsync(int userId);
+
+
+    }
+}
