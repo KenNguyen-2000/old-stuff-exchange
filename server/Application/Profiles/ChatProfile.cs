6bcac640fccf1fee6e552c86a405f8e5aa7def46
Nguyen Kien <ngkien299@gmail.com>
2023-07-09 23:10:43 +0700

    Add mapper Chat services dto

---
+++ server/Application/Profiles/ChatProfile.cs
@@ -0,0 +1,31 @@
+﻿using Application.DTOs;
+using Application.DTOs.ChatDtos;
+using AutoMapper;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Profiles
+{
+    public class ChatProfile : Profile
+    {
+        public ChatProfile()
+        {
+            CreateMap<Message, MessageDto>();
+
+            CreateMap<SendMessageDto, Message>();
+            CreateMap<Message, SendMessageDto>();
+
+            CreateMap<RoomChat, RoomChatDto>()
+                .ForMember(dest => dest.Users, opt => opt.MapFrom(src => src.Users))
+                .ForMember(dest => dest.Messages, opt => opt.MapFrom(src => src.Messages))
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+
+            CreateMap<Message, RoomChatMessageDto>();
+        }
+    }
+}
